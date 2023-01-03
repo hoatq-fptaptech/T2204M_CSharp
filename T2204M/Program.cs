06d57eb6a715c5fb403a713cdd72c38e7d78f01c
macbook <quanghoa.trinh@gmail.com>
2023-01-03 11:31:54 +0700

    first code

---
+++ T2204M/Program.cs
@@ -0,0 +1,51 @@
+﻿using T2204M.session1;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        double y = 3.14;
+        
+        string s = "T2204M";
+
+        int x = 10;
+        if (x >= 10)
+        {
+            Console.WriteLine("A");
+        }else if (x > 5)
+        {
+            Console.WriteLine("B");
+        }else if (x > 15)
+        {
+            Console.WriteLine("C");
+        }
+        else
+        {
+            Console.WriteLine("D");
+        }
+
+        int[] ns = new int[3];
+
+        ns[0] = 15;
+        ns[1] = 20;
+        ns[2] = 25;
+
+        foreach(int n in ns)
+        {
+            //n
+        }
+
+        for(int i = 0; i < ns.Length; i++)
+        {
+            // ns[i]
+        }
+
+        string input = Console.ReadLine(); // scan
+        int num = Convert.ToInt32(input);
+        Console.WriteLine("num = " + num);
+
+        Human h = new Human();
+        h.name = "Nguyen Van An";
+        h.Run();
+    }
+}
---
+++ T2204M/session1/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace T2204M.session1
+{
+	public class Human
+	{
+		public string name;
+		public int age;
+
+		public void Eat()
+		{
+			Console.WriteLine("Eating..");
+		}
+
+		public void Run()
+		{
+			Console.WriteLine(this.name + " running..");
+		}
+	}
+}
+
